ac65fb31fe697826df4904bb1165d53fb20afcb1
Jase Chen <xxmm@live.cn>
2018-09-01 00:31:22 +0800

    _

--- Main.py
+++ Main.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-'''
-# =============================================================================
-#      FileName: Main.py
-#          Desc:
-#        Author: Jase Chen
-#         Email: xxmm@live.cn
-#      HomePage: https://jase.im/
-#       Version: 0.0.1
-#       License: GPLv2
-#    LastChange: 2018-08-30 21:31:51
-#       History:
-# =============================================================================
+'''
+# =============================================================================
+#      FileName: Main.py
+#          Desc:
+#        Author: Jase Chen
+#         Email: xxmm@live.cn
+#      HomePage: https://jase.im/
+#       Version: 0.0.1
+#       License: GPLv2
+#    LastChange: 2018-08-30 21:31:51
+#       History:
+# =============================================================================
 '''
 
 from mainwindow import Ui_MainWindow
@@ -19,6 +19,7 @@ from PyQt5.QtWidgets import QMainWindow, QMessageBox, QApplication, QInputDialog
 from PyQt5 import QtGui
 from PyQt5.QtGui import QIcon, QPixmap
 from time import sleep
+import datetime
 import sqlite3
 import sys
 # import logging
@@ -37,17 +38,13 @@ class Main(QMainWindow):
         self.show()
 
         # 对象属性
-        self.birthday_date = self.ui.birthday_dateEdit.date()
-        self.birth = [
-            self.birthday_date.year(),
-            self.birthday_date.month(),
-            self.birthday_date.day()
-        ]
         # 绑定事件
         self.ui.generate_pushButton.clicked.connect(self.test)
         self.ui.gender_comboBox.currentIndexChanged.connect(
             self.set_retire_age)
         self.ui.retire_age_comboBox.currentIndexChanged.connect(
+            self.calculate_time_to_retire)
+        self.ui.birthday_dateEdit.dateChanged.connect(
             self.calculate_time_to_retire)
 
         # 设置初始值
@@ -57,6 +54,9 @@ class Main(QMainWindow):
         birthday_date = self.ui.birthday_dateEdit.date()
 
     def set_retire_age(self):
+        """
+        根据性别更新退休年龄
+        """
         G_Combo = self.ui.gender_comboBox
         Retire_age_Combo = self.ui.retire_age_comboBox
         if G_Combo.currentText() == "男":
@@ -68,11 +68,49 @@ class Main(QMainWindow):
         self.calculate_time_to_retire()
 
     def calculate_time_to_retire(self):
+        """
+        更新程序中距离退休时间一栏
+        """
+        self.birthday_date = self.ui.birthday_dateEdit.date()
+        self.birth = [
+            self.birthday_date.year(),
+            self.birthday_date.month(),
+            self.birthday_date.day()
+        ]
+        self.birth_date = datetime.date(self.birth[0], self.birth[1],
+                                        self.birth[2])
         if self.ui.retire_age_comboBox.currentText():
             self.retire_age = self.birth.copy()
             self.retire_age[0] += int(
                 self.ui.retire_age_comboBox.currentText())
-            self.ui.time_to_retire_lineEdit.setText(str(self.retire_age))
+            self.retire_date = datetime.date(
+                self.retire_age[0], self.retire_age[1], self.retire_age[2])
+            today = datetime.date.today()
+            retire_date = self.time_to_retire(self.retire_date, today)
+            self.ui.time_to_retire_lineEdit.setText((str(
+                retire_date[0]) + "年" + str(retire_date[1]) + "月").rjust(10))
+            if retire_date[0] < 5:
+                self.ui.time_to_retire_lineEdit.setStyleSheet("color:red")
+            else:
+                self.ui.time_to_retire_lineEdit.setStyleSheet("color:black")
+
+    def time_to_retire(self, retire_date, today_date):
+        """
+        计算离退休还有多久
+        input: 退休日期(retire_date), 今天日期(today_date)
+        type:  datetime.date
+        return:delta_year, delta_month
+        type:  datetime.date
+        """
+        delta_day = retire_date.day - today_date.day
+        delta_month = retire_date.month - today_date.month
+        delta_year = retire_date.year - today_date.year
+        if delta_day > 0:
+            delta_month += 1
+        if delta_month < 0:
+            delta_month += 12
+            delta_year -= 1
+        return delta_year, delta_month
 
     def time_lefted_to_retire(self, gender, birthday, retire_age):
         pass
