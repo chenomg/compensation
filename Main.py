1af5ff30ade9430c2666c88a738d7f06cca268f0
Jase Chen <xxmm@live.cn>
2018-08-31 17:37:10 +0800

    '_'

--- Main.py
+++ Main.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-'''
-# =============================================================================
-#      FileName: Main.py
-#          Desc:
-#        Author: Jase Chen
-#         Email: xxmm@live.cn
-#      HomePage: https://jase.im/
-#       Version: 0.0.1
-#       License: GPLv2
-#    LastChange: 2018-08-30 21:31:51
-#       History:
-# =============================================================================
+'''
+# =============================================================================
+#      FileName: Main.py
+#          Desc:
+#        Author: Jase Chen
+#         Email: xxmm@live.cn
+#      HomePage: https://jase.im/
+#       Version: 0.0.1
+#       License: GPLv2
+#    LastChange: 2018-08-30 21:31:51
+#       History:
+# =============================================================================
 '''
 
 from mainwindow import Ui_MainWindow
 from PyQt5.QtWidgets import QMainWindow, QMessageBox, QApplication, QInputDialog, QLineEdit
 from PyQt5 import QtGui
 from PyQt5.QtGui import QIcon, QPixmap
+from time import sleep
 import sqlite3
 import sys
 # import logging
@@ -34,18 +35,26 @@ class Main(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.show()
+
+        # 对象属性
+        self.birthday_date = self.ui.birthday_dateEdit.date()
+        self.birth = [
+            self.birthday_date.year(),
+            self.birthday_date.month(),
+            self.birthday_date.day()
+        ]
         # 绑定事件
         self.ui.generate_pushButton.clicked.connect(self.test)
-        self.ui.gender_comboBox.currentIndexChanged.connect(self.set_retire_age)
+        self.ui.gender_comboBox.currentIndexChanged.connect(
+            self.set_retire_age)
+        self.ui.retire_age_comboBox.currentIndexChanged.connect(
+            self.calculate_time_to_retire)
 
         # 设置初始值
         self.ui.gender_comboBox.addItems(["男", "女"])
 
     def test(self):
-        print(self.ui.name_lineEdit.text())
         birthday_date = self.ui.birthday_dateEdit.date()
-        print(birthday_date.year())
-        print(self.ui.gender_comboBox.currentText())
 
     def set_retire_age(self):
         G_Combo = self.ui.gender_comboBox
@@ -56,6 +65,14 @@ class Main(QMainWindow):
         if G_Combo.currentText() == "女":
             Retire_age_Combo.clear()
             Retire_age_Combo.addItems(["50", "55"])
+        self.calculate_time_to_retire()
+
+    def calculate_time_to_retire(self):
+        if self.ui.retire_age_comboBox.currentText():
+            self.retire_age = self.birth.copy()
+            self.retire_age[0] += int(
+                self.ui.retire_age_comboBox.currentText())
+            self.ui.time_to_retire_lineEdit.setText(str(self.retire_age))
 
     def time_lefted_to_retire(self, gender, birthday, retire_age):
         pass
